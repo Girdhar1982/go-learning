7b88d5066d5b7acfb4907c8b6098120a34b7e738
Manish Girdhar <manish.girdhar@hotmail.com>
2022-08-17 13:47:59 +1000

    Learn: Pointers

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-//each file must have main 
+//each file must have main
 func main() {
-
-fmt.Println(varibaleAndFunctions())
-value1,value2:=functionreturnTwo()
-fmt.Println(value1,value2)
-
+//fmt.Println(varibaleAndFunctions()) ; value1,value2:=functionreturnTwo(); fmt.Println(value1,value2)
+variablesForPointer()
 }
 
 // Variables and Functions
@@ -26,4 +25,19 @@ func functionreturnTwo() (string,string ){
 	who:="Everyone"
 	whatToSay = "Thank you .."
 	return  (whatToSay + who), "Good bye."
+}
+
+//Pointers
+func variablesForPointer(){
+var myString string
+myString="Green"
+log.Println("myString is Set to," ,myString)
+fmt.Println("Let's change the value using Pointers.")
+changeUsingPointer(&myString)
+log.Println("myString is now Set to," ,myString)
+}
+
+func changeUsingPointer(s *string){ //s is pointer of string
+	log.Println("Address of String ," ,s)
+newValue:="Red"; *s =newValue;
 }

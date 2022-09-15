a49261cfe5371a1e83dbecd8d56d8ea0dcb3c97a
Manish Girdhar <manish.girdhar@hotmail.com>
2022-09-16 09:32:30 +1000

    Learn: json read write

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/girdhar1982/go-learning/helpers"
 	"log"
@@ -16,7 +17,8 @@ func main() {
 	//	mainForDecisionOperators()
 	//mainForLooping()
 	//mainForInterface()
-	mainForChannels()
+	//mainForChannels()
+	mainForJsonReadWrite()
 }
 
 // Variables and Functions
@@ -226,4 +228,60 @@ func mainForChannels() {
 	go CalculateValue(intChan) //Go routine take Channel and run it
 	num := <-intChan //assign to num whatever comes from int channel
 	log.Println(num)
+}
+
+//json read & write
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog bool `json:"has_dog"`
+}
+func mainForJsonReadWrite(){
+	myJson := `
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
+var unmarshalled []Person
+err := json.Unmarshal([]byte(myJson), &unmarshalled)
+if err != nil {
+	log.Println("Error unmarshalling json", err)
+}
+log.Printf("unmarshalled: %v", unmarshalled)
+// write json from a struct
+var mySlice []Person
+
+var m1 Person
+m1.FirstName = "Wally"
+m1.LastName = "West"
+m1.HairColor = "red"
+m1.HasDog = false
+
+mySlice = append(mySlice, m1)
+
+var m2 Person
+m2.FirstName = "Diana"
+m2.LastName = "Prince"
+m2.HairColor = "black"
+m2.HasDog = false
+
+mySlice = append(mySlice, m2)
+
+newJson, err := json.MarshalIndent(mySlice, "", "     ") //MarshalIndent will show the formated json
+if err != nil {
+	log.Println("error marshalling", err)
+}
+
+fmt.Println(string(newJson))  //String to convert slice of byte to String
 }

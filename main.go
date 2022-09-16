c08c0855b3ec496fa541e24143ca89bf6cf0379e
Manish Girdhar <manish.girdhar@hotmail.com>
2022-09-16 13:24:48 +1000

    Learn: start a basic web server

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-
+	"fmt"
+	"net/http"
 )
 
 // each file must have main
 func main() {
-
+http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+	n,err := fmt.Fprintf(w,"Hello, World!")
+	if err!= nil {fmt.Println(err)}
+	fmt.Printf("bytes Written %d\n",n)})
+	http.ListenAndServe(":8080",nil)
 }
